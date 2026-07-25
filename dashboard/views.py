1e742cac373bcc1d98a06a29f08ecfe78c425d0b
Colin Keogh <colin-182@hotmail.com>
2026-07-25 22:29:46 +0100

    pass dashboard stats through view context

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "dashboard/home.html")
+    context = {
+        "project_count": 5,
+        "message_count": 2,
+        "deadline_count": 3,
+    }
+
+    return render(request, "dashboard/home.html", context)
