4b46e1ff3b189cafa2a0c95daa97b1d88d1f3bcb
Colin Keogh <colin-182@hotmail.com>
2026-07-26 18:37:01 +0100

    urls and views.py updated to connect to actual URLs

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 
 
 def home(request):
-    context = {
-        "project_count": 5,
-        "message_count": 2,
-        "deadline_count": 3,
-    }
+    return render(request, "dashboard/home.html")
 
-    return render(request, "dashboard/home.html", context)
+
+def projects(request):
+    return render(request, "dashboard/projects.html")
+
+
+def messages(request):
+    return render(request, "dashboard/messages.html")
+
+
+def profile(request):
+    return render(request, "dashboard/profile.html")
