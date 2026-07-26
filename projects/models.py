c5eb877986c3acd11b6e880d2ed9ab2f87897c0d
Colin Keogh <colin-182@hotmail.com>
2026-07-26 19:20:52 +0100

    created project model for models.py

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,41 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    class Status(models.TextChoices):
+        PLANNING = 'PL', "Planning"
+        ACTIVE = "AC", "Active"
+        COMPLETED = "CO", "Completed"
+
+    title = models.CharField(max_length=200)
+
+    description = models.TextField(
+        blank=True
+    )
+
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="owned_projects",
+    )
+
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.PLANNING,
+    )
+
+    deadline = models.DateField(
+        null=True,
+        blank=True,
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+    
+    def __str__(self):
+        return self.title
