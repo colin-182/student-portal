ba7da87e88ba9def475b9cae5271d60659e2be42
Colin Keogh <colin-182@hotmail.com>
2026-07-28 19:02:20 +0100

    reverting views.py back to original state before debug

--- projects/views.py
+++ projects/views.py
@@ -3,9 +3,6 @@ from .models import Project
 
 
 def project_list(request):
-    print("APP:", request.resolver_match.app_name)
-    print("URL:", request.resolver_match.url_name)
-
     projects = Project.objects.all().order_by("-created_at")
 
     return render(
