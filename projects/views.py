58b48cec09ef319b11655ff95657e982c585a04c
Colin Keogh <colin-182@hotmail.com>
2026-07-28 19:00:43 +0100

    debug for highlight section not working on sidebar. Cache disable on devtools worked so hard reset worked

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,17 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Project
 
 
 def project_list(request):
-    return HttpResponse("✅ NEW PROJECT VIEW IS RUNNING")
+    print("APP:", request.resolver_match.app_name)
+    print("URL:", request.resolver_match.url_name)
+
+    projects = Project.objects.all().order_by("-created_at")
+
+    return render(
+        request,
+        "projects/project_list.html",
+        {
+            "projects": projects,
+        },
+    )
