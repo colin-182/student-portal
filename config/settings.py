bdd3516edcc7d0bc8bc282e77223786a7ebdd096
Colin Keogh <colin-182@hotmail.com>
2026-08-06 22:18:09 +0100

    add django authentication login and logout

--- config/settings.py
+++ config/settings.py
@@ -146,3 +146,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 # --------------------------------------------------
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "dashboard:home"
+
+LOGOUT_REDIRECT_URL = "login"
