3a5b1d32626b433f2d8810f8eed7de89cc97426c
Colin Keogh <colin-182@hotmail.com>
2026-08-10 12:33:32 +0100

    add password reset functionality

--- config/settings.py
+++ config/settings.py
@@ -150,3 +150,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = "dashboard:home"
 
 LOGOUT_REDIRECT_URL = "login"
+
+# --------------------------------------------------
+# Email
+# --------------------------------------------------
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+DEFAULT_FROM_EMAIL = "noreply@studentportal.local"
--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from django.urls import include, path
 from django.contrib.auth import views as auth_views
+from django.urls import include, path
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path(
+        "admin/",
+        admin.site.urls,
+    ),
 
     path(
         "accounts/login/",
@@ -17,8 +21,59 @@ urlpatterns = [
         name="logout",
     ),
 
-    path("", include("dashboard.urls")),
-    path("accounts/", include("accounts.urls")),
-    path("projects/", include("projects.urls")),
-    path("messaging/", include("messaging.urls")),
+    path(
+        "accounts/password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="registration/password_reset_form.html",
+            email_template_name="registration/password_reset_email.html",
+            subject_template_name="registration/password_reset_subject.txt",
+            success_url="/accounts/password-reset/done/",
+        ),
+        name="password_reset",
+    ),
+
+    path(
+        "accounts/password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="registration/password_reset_done.html",
+        ),
+        name="password_reset_done",
+    ),
+
+    path(
+        "accounts/reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="registration/password_reset_confirm.html",
+            success_url="/accounts/reset/done/",
+        ),
+        name="password_reset_confirm",
+    ),
+
+    path(
+        "accounts/reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="registration/password_reset_complete.html",
+        ),
+        name="password_reset_complete",
+    ),
+
+    path(
+        "",
+        include("dashboard.urls"),
+    ),
+
+    path(
+        "accounts/",
+        include("accounts.urls"),
+    ),
+
+    path(
+        "projects/",
+        include("projects.urls"),
+    ),
+
+    path(
+        "messaging/",
+        include("messaging.urls"),
+    ),
 ]
